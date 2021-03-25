c706c8b425d87371ba709dd05b2961edf5bab36c
nwasserman12 <nickyann0823@aol.com>
2021-03-24 20:17:05 -0500

    finished part 1 of assignment validation

--- script.js
+++ script.js
@@ -11,3 +11,25 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function(){
+   let form = document.querySelector("form"); 
+   form.addEventListener("submit", function(event){
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      let fuelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoInput = document.querySelector("input[name=cargoMass]");
+      if (pilotInput.value === '' || copilotInput.value === '' || fuelInput.value === '' || cargoInput.value === ''){
+         alert("All fields are required!"); 
+         event.preventDefault();
+      }
+      if (isNaN(pilotInput.value) === false || isNaN(copilotInput.value) === false){
+         alert("Please enter a valid name!");
+         event.preventDefault(); 
+      }
+      if (isNaN(fuelInput.value) === true || isNaN(cargoInput.value) === true){
+         alert("Please enter a numerical value!");
+         event.preventDefault(); 
+      }
+   });
+});
